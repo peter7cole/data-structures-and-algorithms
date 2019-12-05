a2a096a311795f549336c8f1f7dfc987e4515f73
‘peter-robert-cole’ <petercole3@gmail.com>
2019-12-04 17:51:52 -0800

    four passed

--- code-challenges/sort/challenges-03.test.js
+++ code-challenges/sort/challenges-03.test.js
@@ -97,7 +97,18 @@ For example, ['Alphabet', 'alphabet', 'carrot', 'Zebra'] is correctly sorted, an
 ------------------------------------------------------------------------------------------------ */
 
 const alphabetizeBetter = (arr) => {
-  // Solution code here...
+  arr.sort((a, b) => {
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+    if(a > b){
+      return 1
+    } else if(a < b){
+      return -1
+    } else {
+      return 0
+    }
+  });
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
