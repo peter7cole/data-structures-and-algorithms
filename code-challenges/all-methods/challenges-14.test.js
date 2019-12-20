6ad8f641baa8b326406833d8cbb61fec1100b50b
‘peter-robert-cole’ <petercole3@gmail.com>
2019-12-20 11:32:59 -0800

    four passed

--- code-challenges/all-methods/challenges-14.test.js
+++ code-challenges/all-methods/challenges-14.test.js
@@ -116,7 +116,9 @@ This data could be sorted by name or price.
 ------------------------------------------------------------------------------------------------ */
 
 const sortBy = (property, arr) => {
-  // Solution code here...
+  return property === 'name' ?
+    arr.sort((a, b) => a[property].localeCompare(b[property])) :
+    arr.sort((a, b) => a[property] - b[property]);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -131,9 +133,20 @@ http://www.insecure.com returns false because the URL is not secure
 https://secure.com returns true because the URL is secure
 https:/missingslash.org returns false because the URL is malformed
 ------------------------------------------------------------------------------------------------ */
+
 const isSecure = (url) => {
-// Solution code here...
+  return url.substr(0, 8) === 'https://';
 };
+
+/*
+This one to check the postfix as well
+
+const isSecure = (url) => {
+  let prefix = url.substr(0, 8);
+  let postfix = url.substr(url.length - 4, url.length);
+  return prefix === 'https://' && (postfix === '.net' || postfix === '.com' || postfix === '.org');
+};
+*/
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5 - Stretch Goal
